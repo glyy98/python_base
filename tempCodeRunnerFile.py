af76106b046f6af8861cf3337a7472dd55d14a1f
glyy98 <1007452825@qq.com>
2024-08-29 15:48:17 +0800

    xix

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,10 @@
-shen=str(input("学历："))
-ti=float(input("工作年限"))
-if shen == "本科" :#条件1
-    print("本科通过")
-elif ti>=3:  #条件2
-    print("经验也够")
-else:    #以上条件都不成立执行的
-    print("不符合用人标准")
-
+i=1  
+j=1   
+while i<=10 :
+    while j<5:
+        print(f'符合两个条件i是{i},j是{j}')
+        j=j+1
+    print(f'只符合一个条件i是{i},j是{j}')
+    i=i+1
+    
+print(f'都不符合,i是{i},j是{j}')
